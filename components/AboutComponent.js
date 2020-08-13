49c6889f89c71d0eecd87888e0fa9784f21d8671
fanta <fms294@nyu.edu>
2020-08-13 00:15:16 -0400

    Redux Adding Favorites

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card} from 'react-native-elements';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import {baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return{
@@ -35,6 +36,26 @@ class About extends Component {
                     subtitle={item.description}
                     leftAvatar={{ source: {uri: baseUrl + item.image }}}
                 />
+            );
+        }
+        if(this.props.partners.isLoading){
+            return(
+                <ScrollView>   
+                    <Mission />
+                    <Card title="Community Partners">
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if(this.props.partners.errMess){
+            return(
+                <ScrollView>   
+                    <Mission />
+                    <Card title="Community Partners">
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
             );
         }
         return (
